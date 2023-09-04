1b8837fa898216e83b93b3368760cd43cd5cbe5d
alexChachkhiani <chachkha@gmail.com>
2023-09-04 21:52:38 +0400

    added password strength checker

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ const includeSymbols = document.getElementById("include-symbols");
 const generateButton = document.querySelector("button");
 const password = document.getElementById("password");
 
+let passwordStrength = document.getElementById("password-strength");
+
 function passwordGenerator() {
     let characterSet = "";
+    let countPasswordStrength = 0;
+
     if (uppercase.checked) {
         characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        countPasswordStrength += 1;
     }
 
     if (lowercase.checked) {
         characterSet += "abcdefghijklmnopqrstbwxyz";
+        countPasswordStrength += 1;
     }
 
     if (includeNumbers.checked) {
         characterSet += "0123456789";
+        countPasswordStrength += 1;
     }
 
     if (includeSymbols.checked) {
         characterSet += "~`!@#£€$¢¥§%°^&*()-_+={}[]| \ / :;\"'<>,.?"
+        countPasswordStrength += 1;
     }
 
     let randomPassword = "";
@@ -31,6 +39,21 @@ function passwordGenerator() {
         randomPassword += characterSet[randomIndex];
     }
 
-    password.innerText = randomPassword;
-}
+    if (characterSet != "") {
+        password.innerText = randomPassword;
+    } else {
+        password.innerText = "";
+    }
 
+    if (countPasswordStrength == 0) {
+        passwordStrength.innerText = "NOTHING!"
+    } else if (countPasswordStrength == 1) {
+        passwordStrength.innerText = "TOO WEAK!";
+    } else if (countPasswordStrength == 2) {
+        passwordStrength.innerText = "WEAK!";
+    } else if (countPasswordStrength == 3) {
+        passwordStrength.innerText = "MEDIUM!";
+    } else {
+        passwordStrength.innerText = "STRONG!";
+    }
+}
